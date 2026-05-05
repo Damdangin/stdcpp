4209d1d5e60487f904dc01b5947123174e903259
Damdangin <ttodggs@gmail.com>
2026-05-06 06:38:59 +0900

    2단계 지렁이몸통

--- snake.c
+++ snake.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
 
-#define WIDTH 20
+#define WIDTH 40
 #define HEIGHT 20
 
+int x = 10, y = 10;
+
+void gotoxy(int x, int y) {
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+
+	SetConsoleCursorPosition(consoleHandle, pos);
+}
+
 void draw_map() {
-	// system("cls");
+	gotoxy(0,0);
 
 	for (int i = 0; i < HEIGHT; i++) {
 		for (int j = 0; j < WIDTH; j++) {
 			if (i == 0 || i == HEIGHT - 1 || j == 0 || j == WIDTH -1) {
 				printf("#");
+			} else if (i == y && j == x) {
+				printf("O");
 			} else {
 				printf(" ");
 			}
@@ -20,8 +36,25 @@ void draw_map() {
 }
 
 int main() {
-	draw_map();
-	getchar();
+	char input;
 
-	return 0;
+	system("cls");
+
+	while (1) {
+		draw_map();
+		
+		if (_kbhit()) {
+			input = _getch();
+
+			if(input == 'w') y--;
+			if(input == 's') y++;
+			if(input == 'a') x--;
+			if(input == 'd') x++;
+			if(input == 'q') break;
+		}
+
+		Sleep(10);
+	}
+
+	return 0; //프레임 제한(쓰레드 대기상태)
 }
