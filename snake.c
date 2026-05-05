effd6a660a7737e443843522c3fa751a9345a314
Damdangin <ttodggs@gmail.com>
2026-05-06 08:44:23 +0900

    3단계 꼬리가 몸통따라오는 로직

--- snake.c
+++ snake.c
@@ -6,55 +6,95 @@
 #define WIDTH 40
 #define HEIGHT 20
 
-int x = 10, y = 10;
+int x = 20, y = 10;
+int tailX[100], tailY[100];
+int nTail = 5;
 
-void gotoxy(int x, int y) {
+void gotoxy(int x, int y)
+{
 	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-
-	COORD pos;
-	pos.X = x;
-	pos.Y = y;
-
+	COORD pos = {(short)x, (short)y};
 	SetConsoleCursorPosition(consoleHandle, pos);
 }
 
-void draw_map() {
-	gotoxy(0,0);
+void draw_map()
+{
+	gotoxy(0, 0);
 
-	for (int i = 0; i < HEIGHT; i++) {
-		for (int j = 0; j < WIDTH; j++) {
-			if (i == 0 || i == HEIGHT - 1 || j == 0 || j == WIDTH -1) {
+	for (int i = 0; i < HEIGHT; i++)
+	{
+		for (int j = 0; j < WIDTH; j++)
+		{
+			if (i == 0 || i == HEIGHT - 1 || j == 0 || j == WIDTH - 1)
+			{
 				printf("#");
-			} else if (i == y && j == x) {
+			}
+			else if (i == y && j == x)
+			{
 				printf("O");
-			} else {
-				printf(" ");
+			}
+			else
+			{
+				int isTail = 0;
+				for (int k = 0; k < nTail; k++)
+				{
+					if (tailX[k] == j && tailY[k] == i)
+					{
+						printf("o");
+						isTail = 1;
+						break;
+					}
+				}
+				if (!isTail)
+					printf(" ");
 			}
 		}
 		printf("\n");
 	}
 }
 
-int main() {
+int main()
+{
 	char input;
-
 	system("cls");
 
-	while (1) {
+	while (1)
+	{
 		draw_map();
-		
-		if (_kbhit()) {
-			input = _getch();
 
-			if(input == 'w') y--;
-			if(input == 's') y++;
-			if(input == 'a') x--;
-			if(input == 'd') x++;
-			if(input == 'q') break;
+		if (_kbhit())
+		{
+			input = _getch();
+			int prevX = tailX[0];
+			int prevY = tailY[0];
+			int prev2X, prev2Y;
+			tailX[0] = x;
+			tailY[0] = y;
+
+			for (int i = 1; i < nTail; i++)
+			{
+				prev2X = tailX[i];
+				prev2Y = tailY[i];
+				tailX[i] = prevX;
+				tailY[i] = prevY;
+				prevX = prev2X;
+				prevY = prev2Y;
+			}
+
+			if (input == 'w')
+				y--;
+			if (input == 's')
+				y++;
+			if (input == 'a')
+				x--;
+			if (input == 'd')
+				x++;
+			if (input == 'q')
+				break;
 		}
 
-		Sleep(10);
+		Sleep(50);
 	}
 
-	return 0; //프레임 제한(쓰레드 대기상태)
+	return 0; // 프레임 제한(쓰레드 대기상태)
 }
